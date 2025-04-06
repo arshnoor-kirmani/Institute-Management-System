5dab93f5830b030063aab3523502e95da679e122
Arshnoor Kirmani <personal.arshnoorkirmani@gmail.com>
2025-04-06 08:47:29 +0530

    first commit

---
+++ src/Components/Admin/Exams/Exams.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Exams() {
+  return (
+    <div className="w-full h-screen grid items-center justify-center text-2xl">
+      Exams
+    </div>
+  );
+}
---
+++ src/Components/Admin/FeesReport/FeesReport.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function FeesReport() {
+  return (
+    <div className="w-full h-screen grid items-center justify-center text-2xl">
+      FeesReport
+    </div>
+  );
+}
